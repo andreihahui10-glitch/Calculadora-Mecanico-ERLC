8b3d7c8c02a912d0ade474c8923240e87d37730b
andreihahui10-glitch <andreihahui10@gmail.com>
2026-08-24 15:41:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+// ==============================
+// PRECIOS DEL MECÁNICO
+// ==============================
 
+const precios = {
+    reparacion: 0,
+    pintura: 0,
+    neumaticos: 0,
+    otros: 0
+};
+
+
+// ==============================
+// CALCULAR TOTAL
+// ==============================
+
+function calcular() {
+
+    const reparacion = Number(document.getElementById("reparacion").value);
+    const pintura = Number(document.getElementById("pintura").value);
+    const neumaticos = Number(document.getElementById("neumaticos").value);
+    const otros = Number(document.getElementById("otros").value);
+
+    const total =
+        (reparacion * precios.reparacion) +
+        (pintura * precios.pintura) +
+        (neumaticos * precios.neumaticos) +
+        (otros * precios.otros);
+
+    document.getElementById("total").textContent = "$" + total.toLocaleString();
+}
